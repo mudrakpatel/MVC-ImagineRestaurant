2a9b4366a5ef1552589f5759f215e0a4818dd732
Web1017 <web1017@hotmail.com>
2016-11-20 11:36:16 -0500

    Add project files.

---
+++ MVC-ImagineRestaurant/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_ImagineRestaurant.Startup))]
+namespace MVC_ImagineRestaurant
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
