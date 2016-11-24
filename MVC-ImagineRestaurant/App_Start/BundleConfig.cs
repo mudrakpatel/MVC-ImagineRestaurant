4b7d2247c2291abbbc5b2de8647a7b95292e9a17
Web1017 <web1017@hotmail.com>
2016-11-24 17:05:10 -0500

    Added Menu_List controller

--- MVC-ImagineRestaurant/App_Start/BundleConfig.cs
+++ MVC-ImagineRestaurant/App_Start/BundleConfig.cs
@@ -8,8 +8,11 @@ namespace MVC_ImagineRestaurant
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.IgnoreList.Clear();
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js")
+                        .Include("~/Scripts/app.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -25,8 +28,10 @@ namespace MVC_ImagineRestaurant
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/font-awesome.css",
+                      "~/Content/bootstrap.min.css",
                       "~/Content/site.css"));
+            BundleTable.EnableOptimizations = true;
+            bundles.UseCdn = true;
         }
     }
 }
---
+++ MVC-ImagineRestaurant/Models/Menu_List.cs
@@ -0,0 +1,40 @@
+namespace MVC_ImagineRestaurant.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Menu List")]
+    public partial class Menu_List
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int ItemId { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:C0}")]
+        [Display(Name = "Price")]
+        public int ItemPrice { get; set; }
+
+        [Required]
+        [Display(Name = "Short Description")]
+        [StringLength(100)]
+        public string ShortDescription { get; set; }
+
+        [Required]
+        [Display(Name = "In brief")]
+        [StringLength(450)]
+        public string LongDescription { get; set; }
+
+        [Required]
+        [Display(Name = "Tiny look")]
+        [StringLength(50)]
+        public string IconImage { get; set; }
+
+        [Required]
+        [Display(Name = "Name")]
+        [StringLength(20)]
+        public string ItemName { get; set; }
+    }
+}
